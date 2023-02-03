0be0a7910172322bac7211a825093dbbe2d9295a
mono <mono.romero@gmail.com>
2023-02-02 22:39:22 -0500

    first final version

--- game.js
+++ game.js
@@ -1,10 +1,21 @@
 const canvas = document.querySelector('#game')
-const game = canvas.getContext('2d')
+const message = document.querySelector('#messages')
+const spanLives = document.querySelector('#lives')
+const spanTime = document.querySelector('#time')
+const spanRecord = document.querySelector('#record')
 
 const btnUp = document.querySelector("#up")
 const btnLeft = document.querySelector("#left")
 const btnRight = document.querySelector("#right")
 const btnDown = document.querySelector("#down")
+window.addEventListener('keydown', moveByKeys)
+btnUp.addEventListener("click", moveUp)
+btnLeft.addEventListener("click", moveLeft)
+btnRight.addEventListener("click", moveRight)
+btnDown.addEventListener("click", moveDown)
+
+const game = canvas.getContext('2d')
+
 const canvasLimit = {
   left: 0,
   top: 0,
@@ -20,6 +31,19 @@ const playerPosition = {
   y: undefined
 }
 
+const giftPosition = {
+  x: undefined,
+  y: undefined
+}
+
+let bombPositions = []
+let level = 0
+let lives = 3
+let timeStart
+let timeInterval
+
+
+//set canvas size on load/resize
 window.addEventListener('load', setCanvasSize)
 window.addEventListener('resize', setCanvasSize)
 
@@ -40,18 +64,114 @@ function setCanvasSize(){
   canvasLimit.right = elementsSize * 10
   canvasLimit.bottom = elementsSize * 10
 
+  playerPosition.x = undefined
+  playerPosition.y = undefined
+
   startGame()
+}
+
+function gameWin(){
+  console.log("You Won")
+  cleanCanvas()
+  clearInterval(timeInterval)
+  const FinalTime = spanTime.textContent
+  message.innerHTML= `You won! your time was: ${FinalTime}`
+
+  const recordTime = localStorage.getItem('record_time')
+  const playerTime = Date.now() - timeStart
+
+  if(recordTime){
+    if (recordTime >= playerTime) {
+      localStorage.setItem('record_time', playerTime)
+      message.innerHTML = "New Record!!"
+    } else {
+      message.innerHTML= "No good enough =("
+    }
+  } else{
+    localStorage.setItem('record_time', playerTime)
+    message.innerHTML= "New game and new record"
+  }
+
+  console.log({recordTime, playerTime});
+}
+
+function levelWin(){
+  console.log("Next Level =)");
+  level++
+  startGame()
+}
+
+function levelFail(){
+  if(lives>0){
+    playerPosition.x = undefined
+    playerPosition.y = undefined
+    startGame()
+  } else{
+    startOver()
+  }
+}
+
+function startOver(){
+  playerPosition.x = undefined
+  playerPosition.y = undefined
+  giftPosition.x = undefined
+  giftPosition.y = undefined
+  lives = 3
+  level = 0
+  timeStart = Date.now()
+  startGame()
+}
+
+function showLives(){
+  let heartsArray = Array(lives).fill(emojis['HEART'])
+
+  spanLives.innerHTML = ""
+  heartsArray.forEach(heart=>{
+    spanLives.append(heart)
+  })
+}
+
+function showTime(){
+  spanTime.innerHTML = Date.now() - timeStart
+}
+
+function showRecord(){
+  spanRecord.innerHTML = localStorage.getItem('record_time')
+}
+
+function killTime(){
+
+}
+
+function cleanCanvas(){
+    game.clearRect(0,0,canvasSize, canvasSize)
 }
 
 function startGame(){
 
   game.font = elementsSize*0.8 + 'px arial'
   game.textAlign = 'end'
+  
+  const map = maps[level]
+  //console.log(map);
 
-  //console.log(maps);
+  if(!map){
+    gameWin()
+    return
+  }
+
+  if(!timeStart){
+    timeStart = Date.now()
+    timeInterval = setInterval(showTime, 100)
+    showRecord()
+  }
+
+  showLives()
+
+
 
   //cleans first map (map[0]) and splits by \n
-  const mapRows = maps[0].trim().split('\n')
+  const mapRows = map.trim().split('\n')
 
   //cleans every row of strings then converts strings to array with map()  
   const mapRowCols = mapRows.map(function(row){
@@ -59,10 +179,12 @@ function startGame(){
   })
 
   //cleans all canvas
-  game.clearRect(0,0,canvasSize, canvasSize)
+  cleanCanvas()
+
+  // cleans ememyPosition array
+  bombPositions = []
 
   mapRowCols.forEach((row, rowIndex)=>{
-
     row.forEach((col, colIndex) =>{
   
       const emoji = emojis[col]
@@ -77,25 +199,46 @@ function startGame(){
           playerPosition.x = posX
           playerPosition.y = posY
         }
+      } else if(col == 'I'){
+        giftPosition.x = posX
+        giftPosition.y = posY
+      } else if(col == 'X'){
+        bombPositions.push({x:posX, y:posY})
       }
+
     })
   })
 
   movePlayer()
-  console.log(canvasLimit);
+  //console.log(canvasLimit);
 
 }
 
 function movePlayer(){
-   console.log(playerPosition);
+   //console.log(playerPosition);
     game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y)
-}
 
-window.addEventListener('keydown', moveByKeys)
-btnUp.addEventListener("clik", moveUp)
-btnLeft.addEventListener("clik", moveLeft)
-btnRight.addEventListener("clik", moveRight)
-btnDown.addEventListener("clik", moveDown)
+    const giftCollisionX = playerPosition.x.toFixed(2) == giftPosition.x.toFixed(2)
+    const giftCollisionY = playerPosition.y.toFixed(2) == giftPosition.y.toFixed(2)
+    const isGiftCollision = giftCollisionX && giftCollisionY
+    if(isGiftCollision){
+      levelWin()
+    }
+
+    const bombCollision = bombPositions.find(bomb =>{
+      const bombCollisionX = bomb.x.toFixed(2) == playerPosition.x.toFixed(2)
+      const bombCollisionY = bomb.y.toFixed(2) == playerPosition.y.toFixed(2)
+      return bombCollisionX && bombCollisionY
+    })
+
+    if(bombCollision){
+      console.log('Colission =(');
+      lives--
+      levelFail()
+    }
+
+ 
+}
 
 function moveByKeys(e){
   if(e.key == 'ArrowUp') moveUp()
